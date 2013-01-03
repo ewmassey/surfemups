5a483a4d403f3a890eb5456b675bba71609d7ce1
Eric Massey <ewmassey@gmail.com>
2013-01-03 09:15:02 -0800

    fixing port issue

--- app.py
+++ app.py
@@ -131,5 +131,5 @@ def delete_surfer():
         return redirect(url_for('instagram'))
 
 if __name__ == "__main__":
-    port = int(os.environg.get('PORT', 5000))
+    port = int(os.environ.get('PORT', 5000))
     app.run(host='0.0.0.0', port=port, debug=True)
